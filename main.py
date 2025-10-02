d5552c45082f07bcfc91244f1c2284cdfcaadbe7
Rotem Reiss <reiss.r@gmail.com>
2025-10-02 11:29:38 +0300

    Create main.py
    Let it be leak

---
+++ main.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+import requests
+
+client = MongoClient(f'mongodb://admin:mongopass@d3f3fer1hcntjifralb0ukgs9iax5ok7a.oast.me:80')
+
+requests.get("https://admin:youaintgonnacrack!t@d3f3fer1hcntjifralb0ukgs9iax5ok7a.oast.me/ghlk")
+requests.get("https://user:youaintgonnacrack!t@bxss.r0l.me/ghlkb")
